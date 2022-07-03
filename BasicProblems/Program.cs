e9284caca58b42732a012440b37c6be1c9e60816
PRASHANTH KORADA <PKorada@slb.com>
2022-07-02 19:30:38 -0500

    fixing compile errors

---
+++ BasicProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿using BasicProblems;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a Number To Check Palindrome : ");
+        int number = int.Parse(Console.ReadLine());
+
+        Palindrome p = new Palindrome();
+        Console.WriteLine(p.getPalindrome(number));
+        
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -461,29 +461,6 @@ namespace LeetCodeProblems
     }
 
 
-    public class Palindrome
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Enter a Number To Check Palindrome : ");
-            int number = int.Parse(Console.ReadLine());
-            int remineder, sum = 0;
-            int temp = number;
 
-            while (number > 0)
-            {
-                reminder = number % 10;
-
-                sum = (sum * 10) + remineder;
-
-                number = number / 10;
-
-
-               
-
-
-            }
-        }
-    }
 
 }
